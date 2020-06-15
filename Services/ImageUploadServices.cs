5d8eda9e26c3b454aef9b73233cd0d6dd443008b
Martin <mi_kalonkin@mail.bg>
2020-06-15 22:43:07 +0300

    Create ImageUploadServices.cs

---
+++ Services/ImageUploadServices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Services
+{
+    public class ImageUploadServices
+    {
+        public string UploadImage(ProductCreateViewModel model)
+        {
+            string fileName = model.Photo.FileName;
+            string targetFolder = Path.GetFullPath("~/images");
+            string targetPath = Path.Combine(targetFolder, fileName);
+            using (FileStream stream = new FileStream(targetPath, FileMode.Create))
+            {
+                model.Photo.CopyTo(stream);
+            }
+            return fileName;
+        }
+    }
+}
